0450cd1918230dddb0f589e129f0c66fcf988e6f
Canny <promiseokpoto5050@gmail.com>
2023-12-27 10:09:49 +0100

    my changes

---
+++ src/Components/ServicesData.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PHOTOS from './images/index';
+
+
+const Data = [
+    {
+        id: 1,
+        image: PHOTOS.PHOTO20,
+        heading: "CONTINUOUS EGG PRODUCTION",
+        paragraph: "Our eggs are laid by happy, healthy hens that roam freely on our spacious, sunlit pastures. We collect them daily, ensuring you receive eggs at the peak of freshness. From the moment you crack open one of our eggs, you'll be captivated by the vibrant yolks and the rich aroma that only comes from truly fresh eggs."
+    },
+
+    {
+        id: 2,
+        image: PHOTOS.PHOTO2,
+        heading: "chicken hatchery",
+        paragraph: "Are you looking to start or expand your poultry farm with the healthiest, highest-quality chicks? Look no further. We are dedicated to providing you with top-notch poultry solutions that will set your farm apart from the competition. With our unmatched expertise and commitment to excellence, we guarantee the success and prosperity of your poultry venture."
+    },
+
+    {
+        id: 3,
+        image: PHOTOS.PHOTO4,
+        heading: "continuous fish hatchery",
+        paragraph: "Are you ready to make a splash in the world of aquaculture? Look no further than our cutting-edge fish hatchery, where we turn your aquatic dreams into a thriving reality. With our unwavering commitment to excellence and a passion for sustainable fish farming, we are your trusted partner in achieving remarkable success in the aquaculture industry."
+    },
+
+    {
+        id: 4,
+        image: PHOTOS.PHOTO5,
+        heading: "fish production",
+        paragraph: "With decades of experience in the aquaculture industry, our team of experts brings an unrivaled level of knowledge to the table. We understand the complexities of fish production, from selecting the right species to optimizing growth and maximizing yields. Our seasoned professionals will guide you every step of the way, ensuring that you have the knowledge and support needed to succeed."
+    },
+
+    {
+        id: 5,
+        image: PHOTOS.PHOTO27,
+        heading: "training",
+        paragraph: "Introducing niceoma's Agricultural Training Program — a comprehensive and dynamic curriculum designed to empower farmers, agricultural enthusiasts, and industry professionals. Whether you're a seasoned farmer looking to expand your knowledge or a novice eager to embark on a rewarding agricultural journey, our program has something for everyone."
+    },
+]
+
+
+export default Data;
